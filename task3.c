5a5f76b52a51615af11ed02320fa84727e5a9881
Mahmoud Ehab <eng.mahmoud.e.hussein@gmail.com>
2023-09-18 17:34:43 +0300

    add print octal

--- task3.c
+++ task3.c
@@ -1,10 +1,9 @@
 #include "header.h"
 
 /**
- * 
- * 
- * 
- * 
+ * print_o - pirnt octal number
+ * @n: Unsigned decimal
+ * Return: count
  */
 int print_o(unsigned int n)
 {
@@ -13,20 +12,20 @@ int print_o(unsigned int n)
 
 	i = 0, count = 0;
 	do {
-	    buffer[i++] = n % 8 + '0';
-	    count++;
-	    n /= 8;
+		buffer[i++] = n % 8 + '0';
+		count++;
+		n /= 8;
 	} while (n > 0);
 	buffer[i] = '\0';
 	for (j = 0; j < i / 2; j++)
 	{
-	    temp = buffer[j];
-	    buffer[j] = buffer[i - j - 1];
-	    buffer[i - j - 1] = temp;
+		temp = buffer[j];
+		buffer[j] = buffer[i - j - 1];
+		buffer[i - j - 1] = temp;
 	}
 	for (j = 0; buffer[j] != '\0'; j++)
 	{
-	    write(1, &buffer[j], 1);
+		write(1, &buffer[j], 1);
 	}
 	return (count);
 }

e818d5ab323530e9f71f7207f8d6446018c268c3
Dridi Chaith <dridi.chaith@gmail.com>
2023-09-19 02:26:31 +0000

    change in main

--- main.c
+++ main.c
@@ -9,26 +9,26 @@ int main(void)
 	int n;
 
 	n = _printf("%x\n", 0);
-	printf("%d [length: %d]\n", n, 0);
+	printf("%d [length: %d]\n", 0, n);
 	n = _printf("%x\n", 100);
-	printf("%d [length: %d]\n", n, 100);
+	printf("%d [length: %d]\n", 100, n);
 	n = _printf("%X\n", 100);
-	printf("%d [length: %d]\n", n, 100);
+	printf("%d [length: %d]\n", 100, n);
 	n = printf("%x\n", -100);
-	printf("%d [length: %d]\n", n, -100);
+	printf("%d [length: %d]\n", -100, n);
 	n = _printf("%X\n", -100);
-	printf("%d [length: %d]\n", n, -100);
+	printf("%d [length: %d]\n", -100, n);
 	n = _printf("%x\n", 500);
-	printf("%d [length: %d]\n", n, 500);
+	printf("%d [length: %d]\n", 500, n);
 	n = _printf("%X\n", 500);
-	printf("%d [length: %d]\n", n, 500);
+	printf("%d [length: %d]\n", 500, n);
 	n = _printf("%x\n", INT_MAX);
-	printf("%d [length: %d]\n", n, INT_MAX);
+	printf("%d [length: %d]\n", INT_MAX, n);
 	n = _printf("%X\n", INT_MAX);
-	printf("%d [length: %d]\n", n, INT_MAX);
+	printf("%d [length: %d]\n", INT_MAX, n);
 	n = _printf("%x\n", INT_MIN);
-	printf("%d [length: %d]\n", n, INT_MIN);
+	printf("%d [length: %d]\n", INT_MIN, n);
 	n = _printf("%X\n", INT_MIN);
-	printf("%d [length: %d]\n", n, INT_MIN);
+	printf("%d [length: %d]\n", INT_MIN, n);
 	return (0);
 }
